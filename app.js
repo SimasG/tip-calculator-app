5aaf9c7b761ee2f007ce4b8dd36cafd910e1c9a8
simutis <54304022+SimasG@users.noreply.github.com>
2022-02-17 13:24:22 +0200

    fixed negative value display, custom tip buttom and built reset button functionality

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
 // TODO: must always have two decimals after the number (not more or less) -> DONE
-// TODO: fix value types (no infinity, NaN, etc.) -> TODO
-// TODO: fix negative value display
-// TODO: Fix the custom tix button
-// TODO: Build the reset button functionality
+// TODO: fix value types (no infinity, NaN, etc.) -> DONE
+// TODO: fix negative value display -> DONE
+// TODO: Fix the custom tip button -> DONE
+// TODO: Build the reset button functionality -> DONE
 
 const tipAmountPerPerson = document.querySelector('[data-total-tip-per-person]');
 const totalAmountPerPerson = document.querySelector('[data-total-amount-per-person]');
 const billInputForm = document.querySelector('.bill-input');
 const tipPercentageInput = document.querySelector('.input-tip');
 const peopleNumberInputForm = document.querySelector('.people-number-input');
+const resetBtn = document.querySelector('.reset-button');
 
 function calculateTotals (tipPercentage, people, bill) {
-    let billPerPerson = Number.parseFloat(bill*(1 + tipPercentage) / people).toFixed(2);
-    if (billPerPerson === Infinity || billPerPerson === NaN) {
-        console.log('heyooo infinite bill');
-        billPerPerson = 0;
-        // totalAmountPerPerson.innerHTML = `$0.00`;
-    }
+    let billPerPerson
 
-    if (!Number.isFinite(billPerPerson) && people === 0 || people === '0' || people === '') {
+    if (!Number.isFinite(billPerPerson) && people === 0 || people === '0' || people === '' || people < 0 || isNaN(people)) {
         console.log('bill per person is infinite!')
         billPerPerson = Number.parseFloat(0).toFixed(2);
     } else {
@@ -26,10 +22,15 @@ function calculateTotals (tipPercentage, people, bill) {
         billPerPerson = Number.parseFloat(bill*(1 + tipPercentage) / people).toFixed(2);
     }
 
-    let tipPerPerson = Number.parseFloat((tipPercentage*bill) / people).toFixed(2);
-    if (!Number.isFinite(tipPerPerson) && tipPercentage === 0 || tipPercentage === '0') {
-        console.log('heyooo infinite tip')
-        tipPerPerson = 0;
+
+    let tipPerPerson
+
+    if (!Number.isFinite(tipPerPerson) && tipPercentage === 0 || tipPercentage === '0' || people === 0 || people === '0' || people === '' || people < 0 || isNaN(people)) {
+        console.log('heyooo falsy/zero tip')
+        tipPerPerson = Number.parseFloat(0).toFixed(2);
+    } else {
+        console.log('heyooo not falsy/zero tip')
+        tipPerPerson = Number.parseFloat((tipPercentage*bill) / people).toFixed(2);
     }
 
     totalAmountPerPerson.innerHTML = `$${billPerPerson}`;
@@ -41,7 +42,7 @@ function calculateTotals (tipPercentage, people, bill) {
 // capturing the CUSTOM tip input value into "tipPercentage"
 let tipPercentage = 0;
 tipPercentageInput.addEventListener('input', () => {
-    tipPercentage = tipPercentageInput.value;
+    tipPercentage = (tipPercentageInput.value)/100;
     calculateTotals(tipPercentage, people, total);
 });
 
@@ -68,4 +69,21 @@ let people = 0;
 peopleNumberInputForm.addEventListener('input', () => {
     people = peopleNumberInputForm.value;
     calculateTotals(tipPercentage, people, total);
+})
+
+
+// reset button functionality
+
+// why does the function not work?
+function reset () {
+    totalAmountPerPerson.innerHTML = `$${Number.parseFloat(0).toFixed(2)}`;
+    tipAmountPerPerson.innerHTML = `$${Number.parseFloat(0).toFixed(2)}`;
+    totalAmountPerPerson.value = 0;
+    tipAmountPerPerson.value = 0;
+    peopleNumberInputForm.value = 0;
+    billInputForm.value = 0;
+}
+
+resetBtn.addEventListener('click', () => {
+    reset()
 })
